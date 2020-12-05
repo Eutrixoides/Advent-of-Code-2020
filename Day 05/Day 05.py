99584777b80f88c4c2ac22571f7b81757d0d049d
Eutrixoides <tonykukec@gmail.com>
2020-12-05 06:46:06 +0100

    It's cooler like this (Day 05)
    Replaced 10 strings with 4

--- Day 05/Day 05.py
+++ Day 05/Day 05.py
@@ -2,21 +2,17 @@ inputs = open("input.txt")
 strlist = inputs.read().splitlines()
 seatidlist = []
 for string in strlist:
-    fb = string[:7]
-    rl = string[7:]
-    fb = fb.replace('F','0')
-    fb = fb.replace('B','1')
-    fbint = int(fb,2)
-    #print(fbint)
-    rl = rl.replace('R','1')
-    rl = rl.replace('L','0')
-    rlint = int(rl,2)
-    #print(rlint)
+    fb, rl = string[:7], string[7:]
+    fb = fb.replace('F','0').replace('B','1')
+    rl = rl.replace('R','1').replace('L','0')
+    fbint, rlint = int(fb,2), int(rl,2)
     seatid = fbint*8 + rlint
-    #print(seatid)
     seatidlist.append(seatid)
+
+# Part 1
 print(f"Max is {max(seatidlist)}")
 
+# Part 2
 for seatid in range(min(seatidlist),max(seatidlist)):
     if seatid not in seatidlist:
         print(f'Your seat ID: {seatid}')
